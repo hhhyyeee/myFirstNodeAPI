2e2cc7842b9d1b0cc659e0759b57aa7d5474c5f6
Hyewon Park <changeling626@gmail.com>
2019-03-23 12:33:28 +0900

    Basic CRUD functionality

--- api/cinema/index.js
+++ api/cinema/index.js
@@ -1,21 +1,24 @@
 const express = require('express');
 const router = express.Router();
+var bodyParser = require('body-parser')
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: true }));
 
 let films = [
     {
-        id: 1,
+        id: 0,
         title: 'The Favourite',
         director: 'Yorgos Lanthimos',
         year: '2018'
     },
     {
-        id: 2,
+        id: 1,
         title: 'Carol',
         director: 'Todd Haynes',
         year: '2015'
     },
     {
-        id: 3,
+        id: 2,
         title: 'Lady Bird',
         director: 'Greta Gerwig',
         year: '2017'
@@ -28,7 +31,8 @@ router.get('/', (req, res) => { // root page
 });
 
 router.get('/:id', (req, res) => {
-    const id = parseInt(req.param.id, 10);
+    console.log(req.params.id);
+    const id = parseInt(req.params.id, 10);
     if (!id) {
         return res.status(400).json({error: "Incorrect id"});
     }
@@ -37,8 +41,74 @@ router.get('/:id', (req, res) => {
         return res.status(404).json({error: "Unknown film"});
     }
     return res.json(film);
-})
+});
 
 router.delete('/:id', (req, res) => {
-    
+	console.log(req.params.id);
+	const id = parseInt(req.params.id, 10);
+	if (!id) {
+		return res.status(400).json({error: "Incorrect id"});
+	}
+	const filmIdx = films.findIndex(film => film.id === id);
+	if (filmIdx === -1) {
+		return res.status(404).json({error: "No film found"});
+	}
+	films.splice(filmIdx, 1);
+	res.status(204).send();
+});
+
+router.post('/', (req, res) => {
+	const title = req.body.title || '';
+	if (!title.length) {
+		return res.status(400).json({error: 'Empty title'});
+	}
+	const director = req.body.director || '';
+	if (!director.length) {
+		return res.status(400).json({error: 'Empty director'});
+	}
+	const year = req.body.year || '';
+	if (!year.length) {
+		return res.status(400).json({error: 'Empty year'});
+	}
+	const id = films.reduce((maxId, film) => {
+		return film.id > maxId ? film.id : maxId; // this return value becomes the next maxId value
+	}, 0) + 1; // maxId initial value 0
+	const newFilm = {
+		id: id,
+		title: title,
+		director: director,
+		year: year
+	};
+	films.push(newFilm);
+	return res.status(201).json(newFilm);
 })
+
+router.put('/:id', (req, res) => {
+	console.log(req.params.id);
+	const id = parseInt(req.params.id, 10);
+	if (!id) {
+		return res.status(400).json({error: "Incorrect id"});
+	}
+	let film = films.filter(film => film.id === id)[0];
+    if (!film) {
+        return res.status(404).json({error: "Unknown film"});
+	}
+	const updateFilm = film;
+	if (req.body) {
+		if (req.body.title) {
+			updateFilm.title = req.body.title;
+		}
+		if (req.body.director) {
+			updateFilm.director = req.body.director;
+		}
+		if (req.body.year) {
+			updateFilm.year = req.body.year;
+		}
+		films.push(updateFilm);
+	} else {
+		return res.status(400).json({error: "Nothing to update"});
+	}
+	return res.status(201).json(updateFilm);
+})
+
+module.exports = router;
